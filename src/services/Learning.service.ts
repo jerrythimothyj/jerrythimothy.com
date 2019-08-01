0f959007ed411db5664be26a2a6f13742402800a
Jerry <jerrythimothy@gmail.com>
2019-08-02 00:59:17 +0530

    Loading certificates through service

---
+++ src/services/Learning.service.ts
@@ -0,0 +1,28 @@
+const UDEMY_CERTIFICATES = [
+  [
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-U1MB9VKD.jpg?l=null',
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-CSTO597Y.jpg?l=null',
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-6HWPT24Z.jpg?l=null',
+  ],
+  [
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-U57QTD88.jpg?l=null',
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-CLEKNUX3.jpg?l=null',
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-4K8DENKM.jpg?l=null',
+  ],
+  [
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-1AZMX08U.jpg?l=null',
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-DXONCEEA.jpg?l=null',
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-0FZ4R1LP.jpg?l=en_US',
+  ],
+  [
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-BVGL0BBO.jpg?l=null',
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-CPBH47XC.jpg?l=null',
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-GXVTS2IU.jpg?l=null',
+  ],
+  [
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-5IDF95JF.jpg?l=null',
+    'https://udemy-certificate.s3.amazonaws.com/image/UC-EH67H5SU.jpg?l=null',
+  ],
+]
+
+export const fetchUdemyCertificates = () => UDEMY_CERTIFICATES

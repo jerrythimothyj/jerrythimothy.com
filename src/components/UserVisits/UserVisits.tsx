e1620c9d37718044d25e10379ea918554ac636cc
Jerry Thimothy J <jerrythimothy@gmail.com>
2019-08-02 11:10:09 +0530

    Bug: Visitors world map is not responsive #74 #82
    * Removed unnecessary text
    
    * Flat to globe

--- src/components/UserVisits/UserVisits.tsx
+++ src/components/UserVisits/UserVisits.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const UserVisits: React.FC = () => (
-  <a
-    href="https://www.revolvermaps.com/?target=enlarge&amp;i=53j0oihh6q8"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <img
-      src="https://rf.revolvermaps.com/h/m/a/0/ff0000/256/0/53j0oihh6q8.png"
-      width="512"
-      height="256"
-      alt="Map"
-      style={{ border: 0 }}
-    />
-  </a>
-)
+const UserVisits: React.FC = () => {
+  setTimeout(() => {
+    const placeForGlobe = document.querySelector('#placeForGlobe')
+    const script = document.createElement('script')
+
+    script.src =
+      '//rf.revolvermaps.com/0/0/8.js?i=53j0oihh6q8&amp;m=0&amp;c=ff0000&amp;cr1=ffffff&amp;f=arial&amp;l=33'
+    script.async = true
+
+    placeForGlobe.innerHTML = ''
+    placeForGlobe.appendChild(script)
+  })
+  return <div id="placeForGlobe" />
+}
+
 export default UserVisits
